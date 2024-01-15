4af021ba634447d43310b278994f315989478dcd
bmeinert8 <brian.meinert1@gmail.com>
2024-01-15 08:15:50 -0500

    updates to pwershell script

--- Keyvault.ps1
+++ Keyvault.ps1
@@ -1,10 +1,13 @@
+# Create the Resource Group for the deployment
+$rg = New-AzResourceGroup -Name 'VnetLabeus' -Location 'Eastus2'
+
 # Create the KeyVault in the resource group.
-New-AzKeyVault -Name 'VnetLabKeyVaulteus2' -ResourceGroupName 'VnetLabeus' -Location 'East US 2' -EnabledForDeployment -EnabledForTemplateDeployment
+$vault = New-AzKeyVault -Name 'VnetLabKeyVaulteus2' -ResourceGroupName $rg -Location 'Eastus2' -EnabledForDeployment -EnabledForTemplateDeployment
 
 # Create the admin username and admin password secrets for the vm
 $secret1 = ConvertTo-SecureString 'Username' -AsPlainText -Force
 $secret2 = ConvertTo-SecureString 'Password' -AsPlainText -Force
 
 # Add the secrets to key vault
-Set-AzKeyVaultSecret -VaultName "VnetLabKeyVaulteus2" -Name "AdminUsername" -SecretValue $secret1
-Set-AzKeyVaultSecret -VaultName "VnetLabKeyVaulteus2" -Name "AdminPassword" -SecretValue $secret2
+Set-AzKeyVaultSecret -VaultName $vault -Name "AdminUsername" -SecretValue $secret1
+Set-AzKeyVaultSecret -VaultName $vault -Name "AdminPassword" -SecretValue $secret2
